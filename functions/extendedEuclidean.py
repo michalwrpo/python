c872f3fd8e91bf18423643a8ca27da3be84afcee
michalwrpo <michal.p.walus@gmail.com>
2023-12-04 02:45:35 +0100

    feat: add Extended Euclidean algorithm

---
+++ functions/extendedEuclidean.py
@@ -0,0 +1,9 @@
+
+def exEuclidean(a, b):
+    x, prev_x, y, prev_y = 0, 1, 1, 0
+    while b != 0:
+        q = a//b
+        a, b = b, a % b
+        x, prev_x = prev_x - q * x, x
+        y, prev_y = prev_y - q * y, y
+    return a, prev_x, prev_y
