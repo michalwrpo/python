a44e4cbdb95ed4f971c14bd450f491f73d50c69f
michalwrpo <michal.p.walus@gmail.com>
2023-12-04 03:12:51 +0100

    feat: Euler's phi function calculator

---
+++ functions/eulersPhi.py
@@ -0,0 +1,17 @@
+
+def phi(n: int) -> int:
+    i: int = 3
+    coprime: int = n
+    if n % 2 == 0:
+        coprime = coprime // 2
+        while n % 2 == 0:
+            n = n // 2
+    while i**2 <= n:
+        if n % i == 0:
+            coprime = coprime * (i - 1) // i
+            while n % i == 0:
+                n = n // i
+            i = 1
+        i += 2
+    coprime = coprime * (n-1) // n
+    return coprime
